b08acd667242ca9c1daf888d34b216c984f9974e
Syugo <syu1210.3595@gmail.com>
2025-06-27 21:59:09 +0900

    フィールドサイズを18×36から16×32に変更

--- src/Constants.hpp
+++ src/Constants.hpp
@@ -1,8 +1,8 @@
 #ifndef CONSTANTS_HPP
 #define CONSTANTS_HPP
 //盤面の設定
-#define NUMrow 18//縦の長さ
-#define NUMcol 36//横の長さ
+#define NUMrow 16//縦の長さ
+#define NUMcol 32//横の長さ
 #define NUMmine 60//総地雷数
 //ゲーム状態の分岐
 enum class GameState {
